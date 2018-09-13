7a61cc28bb0c7b72ee5c9302cf84623c7b73da25
Brent Badhwar <bbadh001@ucr.edu>
2018-09-12 20:08:17 -0700

    Fixed off by one space error on first character  when nesting

--- horizontalTree.py
+++ horizontalTree.py
@@ -14,21 +14,21 @@ def syntaxValid(s):
 	for i in range(len(s)):
 		currentChar = s[i]
 		if currentChar == '[':
-			#check for there is a delim before open bracket or trying to double nest:
+			#check for there is a delimiter before open bracket or trying to double nest
 			if i != 0 and s[i-1] == '[' or i > 1 and s[i-2] != ',':
 				return False
 			bracketStack.append('[')
 		elif currentChar == ']':
-			#check if stack is prematurely empty or no delim after closed bracket:
+			#check if stack is prematurely empty or no delimiter after closed bracket
 			if not bracketStack or (i != len(s)-1 and s[i+1] != ']' and s[i+1] != ','):
 				return False 
 			else: 
 				bracketStack.pop()
 		elif currentChar == ',':
-			#check if delim is valid:
+			#check if delimiter is invalid
 			if i == 0 or i == len(s)-1 or s[i-1] == '[' or s[i+1] != ' ':
 				return False
-	#if stack empty, s is valid:
+	#if the stack is empty, we have matched brackets, hence s is valid
 	return len(bracketStack) == 0
 
 def getHorizontalTreeString(s):
@@ -38,14 +38,14 @@ def getHorizontalTreeString(s):
 	Returns:
 		A string of characters
 	"""
-	outputBuffer = []
-	nestingLevel = -1
-	for i in range(len(s)):
+	#we know first character is '[' so we can skip it:
+	outputBuffer = [s[1]]
+	nestingLevel = 0
+	for i in range(2,len(s)):
 		currentChar = s[i]
 		if currentChar == '[':
 			nestingLevel += 1
-			for _ in range(nestingLevel):
-				outputBuffer.append(' ')
+			outputBuffer.append(' ')
 		elif currentChar == ']':
 			nestingLevel -= 1
 		elif currentChar == ',':
@@ -67,7 +67,7 @@ def printAsHorizontalTree(s):
 		print(ERROR_MSG)
 
 def main():
-	s = "[1, 2, [A, B, C, [5, 6]]]" 
+	s = "[1, 2, [A, B, C, [5, 6]]]"
 	printAsHorizontalTree(s)
 
 if __name__ == "__main__":
